3e839dad0b10d6457b2c03c8c21909dc6fc675a7
Lili Bourgeois <lbourgeois90@gmail.com>
2019-04-22 13:02:46 -0500

    routes created

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,6 +16,18 @@ import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
 import AboutPage from '../AboutPage/AboutPage';
 import UserPage from '../UserPage/UserPage';
 import InfoPage from '../InfoPage/InfoPage';
+import AddStudents from '../AddStudents/AddStudents';
+import CreateActivator from '../CreateActivator';
+import ClassActivatorData from '../ClassActivatorData/ClassActivatorData';
+import CreateClasses from '../CreateClasses/CreateClasses';
+import CreateProfile from '../CreateProfile/CreateProfile';
+import CreateUsernameAndPassword from '../CreateUsernameAndPassword/CreateUsernameAndPassword';
+import StudentActivator from '../StudentActivator/StudentActivator';
+import StudentLogin from '../StudentLogin/StudentLogin';
+import Submission from '../Submission/Submission';
+import TeacherLogin from '../TeacherLogin/TeacherLogin';
+import WelcomePage from '../WelcomePage/WelcomePage';
+import LoginSelector from '../LoginSelector';
 
 import './App.css';
 
@@ -36,8 +48,38 @@ class App extends Component {
             This is a route anyone can see, no login necessary */}
             <Route
               exact
-              path="/about"
-              component={AboutPage}
+              path="/loginselector"
+              component={LoginSelector}
+            />
+               <Route
+              exact
+              path="/teacherlogin"
+              component={TeacherLogin}
+            />
+               <Route
+              exact
+              path="/studentlogin"
+              component={StudentLogin}
+            />
+            <Route
+              exact
+              path="/register"
+              component={CreateUsernameAndPassword}
+            />
+             <Route
+              exact
+              path="/createprofile"
+              component={CreateProfile}
+            />
+            <Route
+              exact
+              path="/createclasses"
+              component={CreateClasses}
+            />
+              <Route
+              exact
+              path="/addstudents"
+              component={AddStudents}
             />
             {/* For protected routes, the view could show one of several things on the same route.
             Visiting localhost:3000/home will show the UserPage if the user is logged in.
@@ -45,15 +87,30 @@ class App extends Component {
             Even though it seems like they are different pages, the user is always on localhost:3000/home */}
             <ProtectedRoute
               exact
-              path="/home"
-              component={UserPage}
+              path="/welcome"
+              component={WelcomePage}
             />
             {/* This works the same as the other protected route, except that if the user is logged in,
             they will see the info page instead. */}
             <ProtectedRoute
               exact
-              path="/info"
-              component={InfoPage}
+              path="/createactivator"
+              component={CreateActivator}
+            />
+            <ProtectedRoute
+              exact
+              path="/classdata"
+              component={ClassActivatorData}
+            />
+            <ProtectedRoute
+              exact
+              path="/studentactivator"
+              component={StudentActivator}
+            />
+              <ProtectedRoute
+              exact
+              path="/submission"
+              component={Submission}
             />
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
