b2ff03bdd975b9576b4af185cfb5298e2c717f0b
Lili Bourgeois <lbourgeois90@gmail.com>
2019-04-26 17:10:43 -0500

    playing with styling for welcome page

--- src/components/TeacherSide/WelcomePage/WelcomePage.js
+++ src/components/TeacherSide/WelcomePage/WelcomePage.js
@@ -15,7 +15,6 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import { unstable_Box as Box } from '@material-ui/core/Box';
 
 
 
@@ -30,54 +29,45 @@ class WelcomePage extends Component {
     const { classes } = this.props;
     return (
       <section>
-        <NavBar/>
+        {/* <NavBar/> */}
+        {/* <img src="/images/Activated.png"></img> */}
         <Paper elevation = '0'>
           <Typography className={classes.name} variant="h4">Welcome {this.props.reduxState.teacher.first_name}</Typography>
         </Paper>
         <Grid
           container
-          direction="row"
+          direction="rows"
+          justify="space-between"
+          alignItems="flex-end"
+          spacing={40}
+        >
+        {/* <Grid
+          container
+          direction="rows"
           justify="flex-start"
           alignItems="flex-start"
-          spacing={24}
-        >
-        <Grid item xs={6} >
-        <Box className={classes.boxSize}>
-          <Card className={classes.educationCard} elevation={1}>
-            <CardMedia
-              component="img"
-              alt="Project Screenshot"
-              className={classes.media}
-              image='/images/pencil.png'
-            /> 
-            <CardContent>
-            <Button>Create Activator</Button>
-            </CardContent>
-            
-          </Card>
-          </Box>
+          spacing={8}> */}
+            <Grid
+            item xs={6}>
+              <Paper className={classes.treePaper} elevation={0}><img src="/images/education_tree_concept_.jpg"></img></Paper>
+            </Grid>
+        {/* </Grid> */}
         
-        </Grid>
-        <Grid item xs={6}>
-            <Card className={classes.educationCard} elevation={1}>
-                <CardMedia
-                  component="img"
-                  alt="Project Screenshot"
-                  className={classes.media}
-                  image='/images/graphs.png'
-                /> 
-                <CardContent>
-                <Button>See Class Data</Button>
-                </CardContent>
-                
-                
-              </Card>
-
-
-
+        <Grid 
+          item xs={6}
+          >
         
+            <Grid
+            item xs={6} column>
+              <Paper className={classes.pencilPaper} elevation={0}><img src="/images/lightbulbFormatted.png"></img></Paper>
+            </Grid>
+
+            <Grid
+            item xs={6} column>
+              <Paper className={classes.dataPaper} elevation={0}><img src="/images/graphFormatted.png"></img></Paper>
+            </Grid>
         </Grid>
-        </Grid>
+      </Grid>
 
 
     
@@ -88,27 +78,27 @@ class WelcomePage extends Component {
 }
 
 const styles = theme => ({
-  education: {
-    backgroundImage: "url('/images/educationImage.png')",
-    // backgroundColor: '#D60E54
+
+
+  treePaper:{
+    // height: '100vw',
+    // width: '100%',
+    // backgroundImage: "url('/images/education_tree_concept_.jpg')",
+    // backgroundRepeat: 'no-repeat',
   },
-  media:{
-    height: 'auto',
-    width: '100%',
-    margin:'0 auto',       
+  pencilPaper:{
+    // height: '100vw',
+    // width: '100%',
+    // backgroundImage: "url('/images/create-an-activitor.png')",
+    // backgroundRepeat: 'no-repeat',
   },
-  name: {
-    textAlign: 'center',
-    margin: '2%',
+  dataPaper:{
+    // height: '100vw',
+    // width: '100%',
+    // backgroundImage: "url('/images/graphs.png')",
+    // backgroundRepeat: 'no-repeat',
   },
-  educationCard:{
-    width: '100%',
-    margin: '0 auto',
-    height: '50%',
-  },
-  boxSize:{
-    
-  }
+
 
   })
 
